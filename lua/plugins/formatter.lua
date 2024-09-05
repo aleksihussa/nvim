0ef7fb0a41c5512c1741702d3629d2967a286f1e
Aleksi Hussa <aleksi.hussa@evitec.com>
2024-09-05 19:14:15 +0300

    add common lsp keymaps for nvim-java

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -91,10 +91,10 @@ return { {
   config = function()
     local lint = require("lint")
     lint.linters_by_ft = {
-      -- javascript = { "eslint_d" },
-      -- typescript = { "eslint_d" },
-      -- javascriptreact = { "eslint_d" },
-      -- typescriptreact = { "eslint_d" },
+      javascript = { "eslint_d" },
+      typescript = { "eslint_d" },
+      javascriptreact = { "eslint_d" },
+      typescriptreact = { "eslint_d" },
       markdown = { "vale" }
     }
   end
--- lua/plugins/nvim-java.lua
+++ lua/plugins/nvim-java.lua
@@ -1,13 +1,28 @@
 return {
   "nvim-java/nvim-java",
   config = function()
+    --crete a keymap to go to definition
+    --add keymap for this: require('java').test.view_last_report()
+    vim.api.nvim_set_keymap("n", "<leader>tr", "<cmd>lua require('java').test.view_last_report()<CR>",
+      { noremap = true, silent = true })
+
+
     require("java").setup({
       jdk = {
         auto_install = false
       },
       cmd = { "jdtls" }
     })
-    require("lspconfig").jdtls.setup {
-    }
+    local keymap_opts = { noremap = true, silent = true }
+    require("lspconfig").jdtls.setup({
+      on_attach = function(_, bufnr)
+        vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>",
+          { noremap = true, silent = true })
+        vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", keymap_opts)
+        vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>a", "<cmd>lua vim.lsp.buf.code_action()<CR>", keymap_opts)
+        vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>rr", "<cmd>lua vim.lsp.buf.references()<CR>", keymap_opts)
+        vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, keymap_opts)
+      end,
+    })
   end,
 }
