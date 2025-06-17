877e2ee532ea039e26383b02e64e5a0bd03ee564
Aleksi Hussa <aleksi.hussa@evitec.com>
2025-06-17 18:40:48 +0300

    fix nvim java
    make serialversionuid part of jdtls code actions

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,13 +1,28 @@
-return { {
+return {{
   "ThePrimeagen/harpoon",
   branch = "harpoon2",
   dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
     local harpoon = require("harpoon")
     harpoon.setup()
-    vim.keymap.set("n", "<leader>e", function() harpoon:list():add() end)
-    vim.keymap.set("n", "<leader>o", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-    vim.keymap.set("n", "<C-n>", function() harpoon:list():prev() end)
-    vim.keymap.set("n", "<C-m>", function() harpoon:list():next() end)
+
+    -- Only add mappings when entering a normal (file) buffer
+    vim.api.nvim_create_autocmd("BufEnter", {
+      callback = function(args)
+        local buftype = vim.api.nvim_get_option_value("buftype", { buf = args.buf })
+        if buftype == "" then
+          -- Define buffer-local Harpoon mappings
+          vim.keymap.set("n", "<leader>e", function() harpoon:list():add() end,
+            { desc = "Harpoon add", buffer = args.buf })
+          vim.keymap.set("n", "<leader>o", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end,
+            { desc = "Harpoon menu", buffer = args.buf })
+          vim.keymap.set("n", "<C-n>", function() harpoon:list():prev() end,
+            { desc = "Harpoon prev", buffer = args.buf })
+          vim.keymap.set("n", "<C-m>", function() harpoon:list():next() end,
+            { desc = "Harpoon next", buffer = args.buf })
+        end
+      end
+    })
   end
-} }
+}}
+
--- lua/plugins/nvim-java.lua
+++ lua/plugins/nvim-java.lua
@@ -1,32 +1,63 @@
 return {
   "nvim-java/nvim-java",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+    "mfussenegger/nvim-jdtls",
+    "williamboman/mason.nvim",
+  },
   config = function()
-
     require("java").setup({
       jdk = {
         auto_install = false
       },
-			jdtls = {
-				version = "1.46.1",
-			},
+      jdtls = {
+        enable = true,
+        version = "1.46.1",
+      },
+      java_test = {
+        enable = true,
+        version = "0.43.1",
+      },
+      java_debug_adapter = {
+        enable = true,
+        version = "0.58.1",
+      },
       root_markers = {
-        '.git',
+        ".git",
       },
     })
-    local keymap_opts = { noremap = true, silent = true }
-    require("lspconfig").jdtls.setup({
-      on_attach = function(_, bufnr)
-    vim.api.nvim_set_keymap("n", "<leader>tr", "<cmd>lua require('java').test.view_last_report()<CR>",
-      { noremap = true, silent = true })
-        vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>",
-          { noremap = true, silent = true })
 
-        vim.api.nvim_set_keymap("n", "<A-f>", ":!mvn spotless:apply<CR>", { noremap = true, silent = true })
+    -- Ugly bubblegum workaround but necessary since something is wrong currentyle in the nvim-java and how it combines nvim-jdtls with nvim-java-test internally
+    local function get_java_bundles()
+      local mason_path = vim.fn.stdpath("data") .. "/mason/packages/"
+      local glob = vim.fn.glob
+
+      local test_jars = glob(mason_path .. "java-test/extension/server/*.jar", 1, 1)
+      local debug_jars = glob(mason_path .. "java-debug-adapter/extension/server/*.jar", 1, 1)
+
+      return vim.tbl_extend("force", debug_jars, test_jars)
+    end
+
+    local keymap_opts = { noremap = true, silent = true }
+
+    require("lspconfig").jdtls.setup({
+      settings = {
+        ["java.settings.url"] ="file://" .. vim.fn.expand("~/.config/nvim/rule/java-settings.prefs")
+      },
+      on_attach = function(_, bufnr)
+        vim.api.nvim_set_keymap("n", "<leader>tr", "<cmd>lua require('java').test.view_last_report()<CR>", keymap_opts)
+        vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", keymap_opts)
+        vim.api.nvim_set_keymap("n", "<A-f>", ":!mvn spotless:apply<CR>", keymap_opts)
         vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", keymap_opts)
         vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>a", "<cmd>lua vim.lsp.buf.code_action()<CR>", keymap_opts)
         vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>rr", "<cmd>lua vim.lsp.buf.references()<CR>", keymap_opts)
         vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, keymap_opts)
       end,
+      init_options = {
+        bundles = get_java_bundles()
+      }
     })
   end,
 }
+
